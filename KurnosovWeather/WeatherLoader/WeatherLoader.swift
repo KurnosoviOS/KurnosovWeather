bd9b85b8e6a09000a533036b7761dbf2be7fb5dc
Aleksey <akthesnipe@gmail.com>
2020-02-13 23:04:45 +0500

    WeatherLoader added

---
+++ KurnosovWeather/WeatherLoader/WeatherLoader.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherLoader.swift
+//  KurnosovWeather
+//
+//  Created by Admin on 13.02.2020.
+//  Copyright © 2020 akthesnipe. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+public class WeatherLoader {
+    
+    private var _scheduler = ConcurrentDispatchQueueScheduler(qos: .utility)
+    
+    private static func _httpServiceUrl(cityName: String) -> URL {
+        let urlString = "api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=\(_httpServiceUserId)"
+        
+        return URL(string: urlString)!;
+    }
+    
+    private static let _httpServiceUserId = "c0cae2f38b7d6c324a47eff0a57b1488"
+    
+    public func getWeatherLoader(city: String) -> Observable<Any> {
+        return URLSession.shared.rx.json(url: WeatherLoader._httpServiceUrl(cityName: city))
+            //.timeout(RxTimeInterval.milliseconds(2000), scheduler: _scheduler)
+            .subscribeOn(_scheduler)
+    }
+}

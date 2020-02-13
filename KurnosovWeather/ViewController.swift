6e8eebf65aa547e91dfdba9728d5abd91792883c
Aleksey <akthesnipe@gmail.com>
2020-02-13 20:06:21 +0500

    Initial Commit

---
+++ KurnosovWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KurnosovWeather
+//
+//  Created by Admin on 13.02.2020.
+//  Copyright © 2020 akthesnipe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
